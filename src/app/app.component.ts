4be0e46cccdac4aba190f08dc76cafaba53e00b8
enrique gonzalez <egonzalez@handtevy.com>
2022-11-14 13:15:33 -0500

    Implemented logic to loop over a sound.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
+import { Howler, Howl } from 'howler';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  playing = false;
+  private howl: Howl;
+  task = [];
+  constructor() {
+    Howler.autoUnlock = true;
+    Howler.volume(1.0);
+    Howler.autoSuspend = false;
+    this.howl = new Howl({
+      src: "assets/beep.mp3",
+      preload: true,
+      html5: false
+    });
+    this.howl.once('load', () => console.log('audio file loaded.'));
+    this.howl.once('loaderror', error => console.error(error));
+  }
+
+  toggleSound(): void {
+    this.playing = !this.playing;
+    this.howl.rate(2);
+
+    const runLoop = () => {
+      if (!this.playing) {
+        return;
+      }
+
+      this.howl.once('end', () => runLoop());
+      this.howl.play();
+    };
+
+    runLoop();
+  }
 }
